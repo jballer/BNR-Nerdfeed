4b60e24c564b88fc4e0a1465478c9f7ed4a8e023
Jonathan Ballerano <jballer@jballer.com>
2013-06-25 00:32:50 -0400

    Initial Commit

---
+++ Nerdfeed/NFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NFAppDelegate.h
+//  Nerdfeed
+//
+//  Created by Jonathan Ballerano on 6/25/13.
+//  Copyright (c) 2013 jballer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nerdfeed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nerdfeed
+//
+//  Created by Jonathan Ballerano on 6/25/13.
+//  Copyright (c) 2013 jballer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([NFAppDelegate class]));
+	}
+}
